d87d2db085c14818156676885c233ba300714d2b
Tomislav Habalija <tomislav.habalija@infinum.hr>
2024-12-09 22:43:07 +0100

    Fix routing

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  trailingSlash: true,
   output: "export",
-  /* config options here */
 };
 
 export default nextConfig;
