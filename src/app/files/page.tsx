da763761c8e76a7904048c5eca51897be59e33c3
Tomislav Habalija <tomislav.habalija@infinum.hr>
2024-12-09 23:47:05 +0100

    Move file upload

--- src/app/files/page.tsx
+++ src/app/files/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Container } from "@chakra-ui/react";
-import { FileUpload } from "../../core/file-upload/FileUpload/FileUpload";
+import { FileUpload } from "../../features/file-upload/FileUpload/FileUpload";
 import { FileList } from "../../features/file-list/FileList/FileList";
 
 export default function Files() {
