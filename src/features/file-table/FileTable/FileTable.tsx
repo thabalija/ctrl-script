a8e528435a41a79bd8fda11230346476aef0daf5
Tomislav Habalija <tomislav.habalija@infinum.hr>
2024-12-22 00:54:51 +0100

    Update styles

--- src/features/file-table/FileTable/FileTable.tsx
+++ src/features/file-table/FileTable/FileTable.tsx
@@ -2,13 +2,14 @@
 
 import { FileItem } from "../../../../db";
 
-import { IconButton, Link, Table } from "@chakra-ui/react";
-import { FaRegTrashAlt } from "react-icons/fa";
-import { FaDownload, FaRegPenToSquare } from "react-icons/fa6";
+import { Code, IconButton, Table } from "@chakra-ui/react";
+import { redirect } from "next/navigation";
+import { FiDownload, FiPenTool } from "react-icons/fi";
+import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { downloadFile } from "../../../helpers/download-file";
 
 export interface IFileTableProps {
-  files?: Array<FileItem>;
+  files: Array<FileItem>;
   onDeleteFileItem: (fileItem: FileItem) => void;
 }
 
@@ -17,56 +18,63 @@ export function FileTable({ files, onDeleteFileItem }: IFileTableProps) {
     downloadFile(fileItem.file, `${fileItem.name}.${fileItem.extension}`);
   }
 
-  const emptyState = <p>Loading...</p>;
+  function onEditFileItem(fileItem: FileItem) {
+    redirect(`/editor?fileId=${fileItem.id}`);
+  }
 
-  const rows = (files || []).map((fileItem) => (
+  const rows = files.map((fileItem) => (
     <Table.Row key={fileItem.id}>
-      <Table.Cell>{fileItem.name}</Table.Cell>
-      <Table.Cell>{fileItem.extension}</Table.Cell>
+      <Table.Cell fontWeight="bold">{fileItem.name}</Table.Cell>
+      <Table.Cell>
+        <Code>{fileItem.extension}</Code>
+      </Table.Cell>
       <Table.Cell>
         {new Date(fileItem.file.lastModified).toDateString()}
       </Table.Cell>
-      <Table.Cell>
+      <Table.Cell textAlign="center">
         <IconButton
-          onClick={() => onDeleteFileItem(fileItem)}
+          colorPalette="purple"
           rounded="full"
           variant="ghost"
+          onClick={() => onEditFileItem(fileItem)}
         >
-          <FaRegTrashAlt />
+          <FiPenTool />
         </IconButton>
 
-        <Link href="/editor">
-          <IconButton rounded="full" variant="ghost">
-            <FaRegPenToSquare />
-          </IconButton>
-        </Link>
-
         <IconButton
-          onClick={() => onDownloadFileItem(fileItem)}
+          colorPalette="green"
           rounded="full"
           variant="ghost"
+          onClick={() => onDownloadFileItem(fileItem)}
         >
-          <FaDownload />
+          <FiDownload />
+        </IconButton>
+
+        <IconButton
+          colorPalette="red"
+          rounded="full"
+          variant="ghost"
+          onClick={() => onDeleteFileItem(fileItem)}
+        >
+          <IoMdRemoveCircleOutline />
         </IconButton>
       </Table.Cell>
     </Table.Row>
   ));
 
-  const table = (
+  return (
     <>
-      <Table.Root>
+      <Table.Root variant="outline">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeader>Name</Table.ColumnHeader>
             <Table.ColumnHeader>Type</Table.ColumnHeader>
             <Table.ColumnHeader>Last modified</Table.ColumnHeader>
-            <Table.ColumnHeader>Actions</Table.ColumnHeader>
+            <Table.ColumnHeader textAlign="center">Actions</Table.ColumnHeader>
           </Table.Row>
         </Table.Header>
         <Table.Body>{rows}</Table.Body>
       </Table.Root>
     </>
   );
-
-  return files === undefined ? emptyState : table;
 }
--- src/features/header/Header/Header.tsx
+++ src/features/header/Header/Header.tsx
@@ -1,34 +1,55 @@
 "use client";
 
 import { Container, Link } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
 import { ThemeSwitcher } from "../../../components/ui/theme-switcher";
 
 export function Header() {
+  const pathname = usePathname();
+
+  const links = [
+    { path: "/files/", label: "Files" },
+    { path: "/scripts/", label: "Scripts" },
+    { path: "/editor/", label: "Editor" },
+    { path: "/bulk-editor/", label: "Bulk Editor" },
+  ];
+
   return (
     <Container
       display={"flex"}
       justifyContent={"space-between"}
+      alignItems="center"
       padding="12px 24px"
     >
-      <Container padding="0" flex="1 0 120px">
-        <Link href="/" p="4px 12px">
+      <Container padding="0" flex="1 0 150px">
+        <Link
+          href="/"
+          p="4px 12px"
+          fontWeight="bold"
+          fontSize="20px"
+          letterSpacing="-1px"
+          fontFamily="monospace"
+        >
           CTRL Script
         </Link>
       </Container>
 
       <Container padding="0">
-        <Link href="/files" p="4px 12px">
-          Files
-        </Link>
-        <Link href="/scripts" p="4px 12px">
-          Scripts
-        </Link>
-        <Link href="/editor" p="4px 12px">
-          Editor
-        </Link>
-        <Link href="/bulk-editor" p="4px 12px">
-          Bulk Editor
-        </Link>
+        {links.map((link) => (
+          <Link
+            fontWeight={pathname === link.path ? "bold" : "normal"}
+            key={link.path}
+            href={link.path}
+            p="4px 12px"
+            color={
+              pathname === link.path
+                ? "purple.500"
+                : { _light: "gray.600", _dark: "gray.300" }
+            }
+          >
+            {link.label}
+          </Link>
+        ))}
       </Container>
 
       <Container padding="0" flex="1 0 200px">
