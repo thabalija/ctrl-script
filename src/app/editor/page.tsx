0dec6c19437796c639305c2247d0ec032fd535d0
Tomislav Habalija <tomislav.habalija@infinum.hr>
2024-12-22 02:19:40 +0100

    Update editor

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import { Button, Container } from "@chakra-ui/react";
-import { Theme } from "@monaco-editor/react";
+import {
+  Box,
+  Button,
+  Code,
+  Container,
+  Heading,
+  HStack,
+  Input,
+  Text,
+} from "@chakra-ui/react";
 import { useLiveQuery } from "dexie-react-hooks";
-import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import { FaRegSave } from "react-icons/fa";
+import { LuListStart } from "react-icons/lu";
 import { db } from "../../../db";
+import { Field } from "../../components/ui/field";
 import { Dropdown } from "../../core/select/dropdown";
 import { CodeEditor } from "../../features/editor/CodeEditor/CodeEditor";
 import { FileDiff } from "../../features/editor/FileDiff/FileDiff";
 
 export default function Editor() {
-  const { theme } = useTheme();
-  const codeTheme: Theme = theme === "dark" ? "vs-dark" : "light";
-
   const files = useLiveQuery(() => db.files.toArray()) || [];
   const scripts = useLiveQuery(() => db.scripts.toArray()) || [];
 
@@ -59,15 +66,14 @@ export default function Editor() {
 
   const [selectedFiles, setSelectedFiles] = useState<Array<File>>([]);
 
-  function onFileSelect(selectedFiles: Array<File>) {
+  async function onFileSelect(selectedFiles: Array<File>) {
     const file = selectedFiles[0];
     if (!file) return;
 
-    file.text().then((value) => {
-      setOriginal(value);
-      setModified(value);
-      setSelectedFiles([file]);
-    });
+    const value = await file.text();
+    setOriginal(value);
+    setModified(value);
+    setSelectedFiles([file]);
   }
 
   const scriptOptions = scripts.map((script) => ({
@@ -89,43 +95,73 @@ export default function Editor() {
 
   return (
     <Container>
-      <Dropdown
-        options={fileOptions}
-        selectedValues={selectedFiles}
-        onValueChange={onFileSelect}
-        placeholder="Select file"
-        label="Files"
-      />
+      <Box maxWidth="620px">
+        <Heading as="h1" size="3xl" marginBottom="6">
+          Single file editor
+        </Heading>
+        <Text marginBottom="6">
+          Write <strong>javascript</strong> code to apply to the file. File
+          selected below is provided as the first argument to the function,
+          access it through
+          <Code margin="0 4px">arguments[0]</Code>. Return the modified file.
+        </Text>
+      </Box>
+      <HStack marginBottom="6" alignItems="end">
+        <Dropdown
+          options={fileOptions}
+          selectedValues={selectedFiles}
+          onValueChange={onFileSelect}
+          placeholder="Select file"
+          label="Files"
+        />
 
-      <Dropdown
-        options={scriptOptions}
-        selectedValues={selectedScripts}
-        onValueChange={onScriptSelect}
-        placeholder="Select script"
-        label="Scripts"
-      />
-      <CodeEditor
-        script={script}
-        language={language}
-        theme={codeTheme}
-        handleScriptChange={handleScriptChange}
-      />
-      <br />
-      <Button mb="12px" onClick={executeValue}>
-        Execute value
+        <Dropdown
+          options={scriptOptions}
+          selectedValues={selectedScripts}
+          onValueChange={onScriptSelect}
+          placeholder="Select script"
+          label="Scripts"
+        />
+        <Field label="Script name" maxWidth={300}>
+          <Input placeholder="Enter name..." />
+        </Field>
+        <Button colorPalette="purple" variant="ghost">
+          <FaRegSave />
+          Save script
+        </Button>
+      </HStack>
+      <Box marginBottom="8">
+        <CodeEditor
+          script={script}
+          language={language}
+          handleScriptChange={handleScriptChange}
+        />
+      </Box>
+      <Button marginBottom="12" onClick={executeValue} colorPalette="purple">
+        <LuListStart />
+        Apply script
       </Button>
-      <br />
-      <FileDiff
-        original={original}
-        modified={modified}
-        theme={codeTheme}
-        language={language}
-      />
+      <Box maxWidth="620px">
+        <Heading as="h1" size="3xl" marginBottom="6">
+          File difference
+        </Heading>
+        <Text marginBottom="6">
+          Execute the script to apply the changes to the original file.
+        </Text>
+      </Box>
+      <Box marginBottom="8">
+        <FileDiff
+          original={original}
+          modified={modified}
+          language={selectedFiles[0]?.name.split(".").pop() || ""}
+        />
+      </Box>
     </Container>
   );
 }
 
 const defaultScript = `// File is located in the first argument. Return the modified file.
-const file = arguments[0];
-return file.split(',').map((fruit, i) => fruit + i).join(',');
+const selectedFile = arguments[0];
+console.log(selectedFile);
+return selectedFile;
 `;
