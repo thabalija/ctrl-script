39d999bf33d3807142cfff4e722efcfdeacb428d
Tomislav Habalija <tomislav.habalija@infinum.hr>
2024-12-26 13:24:01 +0100

    Update loader styles

--- src/core/loader/Loader.tsx
+++ src/core/loader/Loader.tsx
@@ -4,7 +4,7 @@ import { Flex, Spinner } from "@chakra-ui/react";
 
 export function Loader() {
   return (
-    <Flex justifyContent="center" alignItems="center" height="100px">
+    <Flex alignItems="center" justifyContent="center" height="100px">
       <Spinner size="lg" color="colorPalette.600" colorPalette="purple" />
     </Flex>
   );
