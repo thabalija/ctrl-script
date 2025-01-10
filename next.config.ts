402c17fc227ccf47d1f5789708d1f40a3bf95686
Tomislav Habalija <tomislav.habalija@infinum.hr>
2025-01-10 07:42:43 +0100

    Add trailing slashes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  trailingSlash: true,
   output: "export",
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
